7c81e9f69547e64e7d0eca3b025c69d632c09c8f
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-07-06 20:14:05 +0900

    implement a simple sample

---
+++ src/main/java/jp/k_ui/test/thymeleaf/Sample.java
@@ -0,0 +1,23 @@
+package jp.k_ui.test.thymeleaf;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		TemplateResolver resolver = new ClassLoaderTemplateResolver();
+		resolver.setTemplateMode("HTML5");
+		
+		TemplateEngine engine = new TemplateEngine();
+		engine.setTemplateResolver(resolver);
+		
+		Context context = new Context();
+		context.setVariable("title", "Hello, Thymeleaf");
+		
+		System.out.println(engine.process("foo.html", context));
+	}
+
+}
